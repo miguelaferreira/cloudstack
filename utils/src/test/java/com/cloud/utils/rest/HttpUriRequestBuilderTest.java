cae41f92737dd36649c8a5c3a503861c5135ed9f
Miguel Ferreira <miguelferreira@me.com>
2015-08-21 17:44:57 +0200

    Add basic RestClient implentation based on HTTP Components 4.5
    - Upgrade version of HTTP Components to 4.5
    - Add helper class to create Http clients
    - Add helper class to build http requests
    - Add enum with the different Http Methods
    - Add constants class for HTTP related values

---
+++ utils/src/test/java/com/cloud/utils/rest/HttpUriRequestBuilderTest.java
@@ -0,0 +1,96 @@
+package com.cloud.utils.rest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+
+import java.util.HashMap;
+
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.junit.Test;
+
+import com.google.common.base.Optional;
+
+public class HttpUriRequestBuilderTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testBuildWithNullMethod() throws Exception {
+        HttpUriRequestBuilder.create().path("/path").build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testBuildWithNullPath() throws Exception {
+        HttpUriRequestBuilder.create().method(HttpMethods.GET).build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testBuildWithEmptyPath() throws Exception {
+        HttpUriRequestBuilder.create()
+            .method(HttpMethods.GET)
+            .path("")
+            .build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testBuildWithIlegalPath() throws Exception {
+        HttpUriRequestBuilder.create()
+            .method(HttpMethods.GET)
+            .path("path")
+            .build();
+    }
+
+    @Test
+    public void testBuildSimpleRequest() throws Exception {
+        final HttpUriRequest request = HttpUriRequestBuilder.create()
+            .method(HttpMethods.GET)
+            .path("/path")
+            .build();
+
+        assertThat(request, notNullValue());
+        assertThat(request.getURI().getPath(), equalTo("/path"));
+        assertThat(request.getURI().getScheme(), nullValue());
+        assertThat(request.getURI().getQuery(), nullValue());
+        assertThat(request.getURI().getHost(), nullValue());
+        assertThat(request.getMethod(), equalTo(HttpGet.METHOD_NAME));
+    }
+
+    @Test
+    public void testBuildRequestWithParameters() throws Exception {
+        final HashMap<String, String> parameters = new HashMap<String, String>();
+        parameters.put("key1", "value1");
+        final HttpUriRequest request = HttpUriRequestBuilder.create()
+            .method(HttpMethods.GET)
+            .path("/path")
+            .parameters(parameters)
+            .build();
+
+        assertThat(request, notNullValue());
+        assertThat(request.getURI().getPath(), equalTo("/path"));
+        assertThat(request.getURI().getQuery(), equalTo("key1=value1"));
+        assertThat(request.getURI().getScheme(), nullValue());
+        assertThat(request.getURI().getHost(), nullValue());
+        assertThat(request.getMethod(), equalTo(HttpGet.METHOD_NAME));
+    }
+
+    @Test
+    public void testBuildRequestWithJsonPayload() throws Exception {
+        final HttpUriRequest request = HttpUriRequestBuilder.create()
+            .method(HttpMethods.GET)
+            .path("/path")
+            .jsonPayload(Optional.of("{'key1':'value1'}"))
+            .build();
+
+        assertThat(request, notNullValue());
+        assertThat(request.getURI().getPath(), equalTo("/path"));
+        assertThat(request.getURI().getScheme(), nullValue());
+        assertThat(request.getURI().getQuery(), nullValue());
+        assertThat(request.getURI().getHost(), nullValue());
+        assertThat(request.getMethod(), equalTo(HttpGet.METHOD_NAME));
+        assertThat(request.containsHeader(HttpConstants.CONTENT_TYPE), equalTo(true));
+        assertThat(request.getFirstHeader(HttpConstants.CONTENT_TYPE).getValue(), equalTo(HttpConstants.JSON_CONTENT_TYPE));
+        assertThat(request, HttpUriRequestPayloadMatcher.hasPayload("{'key1':'value1'}"));
+    }
+
+}
