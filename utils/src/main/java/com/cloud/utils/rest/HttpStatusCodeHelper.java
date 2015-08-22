910a38ee79de8af8207110701cf57137f19d164c
Miguel Ferreira <miguelferreira@me.com>
2015-08-22 17:05:30 +0200

    Add NSX specific RestClient implementation
    - Add -noverify JVM arg to surefire plugin, to allow Powermockito to
      de-encapsulate private methods
    - Add dependency on cloud-utils test-jar to use custom HttpRequest
      matchers

---
+++ utils/src/main/java/com/cloud/utils/rest/HttpStatusCodeHelper.java
@@ -0,0 +1,15 @@
+package com.cloud.utils.rest;
+
+import org.apache.http.HttpStatus;
+
+public class HttpStatusCodeHelper {
+
+    public static boolean isSuccess(final int statusCode) {
+        return statusCode >= HttpStatus.SC_OK && statusCode <= HttpStatus.SC_MULTI_STATUS;
+    }
+
+    public static boolean isUnauthorized(final int statusCode) {
+        return statusCode == HttpStatus.SC_UNAUTHORIZED;
+    }
+
+}
