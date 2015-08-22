f349f5789e9d4470c79c0a87432f85988c7deaa1
Miguel Ferreira <miguelferreira@me.com>
2015-08-22 17:30:58 +0200

    Use NSX specific RestClient in API implementation (NiciraNvpApi)
    - Simplify public API to return Lists instead of NiciraNvpLists

---
+++ utils/src/test/java/com/cloud/utils/rest/HttpUriRequestPathMatcher.java
@@ -0,0 +1,24 @@
+package com.cloud.utils.rest;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Matchers.argThat;
+
+import org.apache.http.client.methods.HttpUriRequest;
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
+
+public class HttpUriRequestPathMatcher extends FeatureMatcher<HttpUriRequest, String> {
+
+    public static HttpUriRequest aPath(final String path) {
+        return argThat(new HttpUriRequestPathMatcher(equalTo(path), "path", "path"));
+    }
+
+    public HttpUriRequestPathMatcher(final Matcher<? super String> subMatcher, final String featureDescription, final String featureName) {
+        super(subMatcher, featureDescription, featureName);
+    }
+
+    @Override
+    protected String featureValueOf(final HttpUriRequest actual) {
+        return actual.getURI().getPath();
+    }
+}
