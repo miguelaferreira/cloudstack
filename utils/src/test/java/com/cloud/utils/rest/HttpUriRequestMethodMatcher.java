b9f05409f49a6ba7c1014aa2e9ea3bb41cb85edd
Miguel Ferreira <miguelferreira@me.com>
2015-08-23 15:11:29 +0200

    Delegate HTTP protocol activity in RESTServiceConnector to RestClient
    - All HTTP protocol activities are now handled by RestClient
    - This service is now only responsible for creating requests, and
      dispatching them to the client
    - Provides a Simple API for creating, updating, retrieving and deleting
      objects

---
+++ utils/src/test/java/com/cloud/utils/rest/HttpUriRequestMethodMatcher.java
@@ -0,0 +1,25 @@
+package com.cloud.utils.rest;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Matchers.argThat;
+
+import org.apache.http.client.methods.HttpUriRequest;
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
+
+public class HttpUriRequestMethodMatcher extends FeatureMatcher<HttpUriRequest, String> {
+
+    public static HttpUriRequest aMethod(final String method) {
+        return argThat(new HttpUriRequestMethodMatcher(equalTo(method), "method", "method"));
+    }
+
+    public HttpUriRequestMethodMatcher(final Matcher<? super String> subMatcher, final String featureDescription, final String featureName) {
+        super(subMatcher, featureDescription, featureName);
+    }
+
+    @Override
+    protected String featureValueOf(final HttpUriRequest actual) {
+        return actual.getMethod();
+    }
+
+}
